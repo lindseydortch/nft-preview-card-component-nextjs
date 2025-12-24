e621f0d1405a9b1134e39cd207d1d036cea2b3f5
“Lindsey <codewithlinds@gmail.com>
2025-12-24 11:30:28 -0600

    Initial Repo setup

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Card from "@/app/_components/card/Card";
+import styles from "./home.module.scss";
+
+export default function Home() {
+  return (
+    <section className={styles.section}>
+      <Card></Card>
+    </section>
+  );
+}
